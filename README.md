b78f91ea11b67e8ed95c95e49af78447e328078c
David Rauf <davidrauf@gmail.com>
2024-06-17 11:08:07 -0400

    working with readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ExpressNoteTaker
 Using Express to generate a note taking app
+
+This app allows for the user to write and save notes to a JSON file. The deployed app allows user to view saved notes as well as create additions;
+
+GitHub Repo:
+https://github.com/drauf727/ExpressNoteTaker
+
+GitHub Page:
+
+https://drauf727.github.io/ExpressNoteTaker/
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
-
-// const api = require('./routes/index');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const noteData = './db/db.json';
 
 const PORT = 5000;
 
@@ -12,13 +13,54 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-// app.use('./api', api);
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile(noteData, 'utf8', (err, data) => {
+        let noteList = [];
+        if (err) {
+            console.log("Unable to read notes");
+            return;
+        }
+        console.log(data);
+        if (data) {
+            noteList = JSON.parse(data);
+        }
+        res.json(noteList);
+    })
+});
+
+app.post('/api/notes', (req, res) => {
+    const { text, title } = req.body;
+    const postNote = {
+        text: text,
+        title: title,
+        id: uuidv4()
+    };
+    fs.readFile(noteData, 'utf8', (err, data) => {
+        const noteList = JSON.parse(data);
+        if (err) {
+            console.log("Unable to read notes");
+            return;
+        }
+        noteList.push(postNote);
+        fs.writeFile(noteData, JSON.stringify(noteList, null, 2), (err) => {
+            if (err) {
+                console.log("Error writing note to file");
+                return;
+            }
+            console.log("Success");
+        })
+    })
+    res.json(postNote);
+})
+
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`)
 );
